c24991fd7926c9de446c2f78a7f16ddd129ac2d3
AkiShiroshita <akihirokun8@gmail.com>
2022-06-20 03:27:41 +0000

    meta-epi

--- cleaning/cleaning.R
+++ cleaning/cleaning.R
@@ -13,7 +13,30 @@ packages = c("devtools",
              "tidylog",
              "ggplotgui",
              "ggthemes",
-             "arsenal")
+             "scales",
+             "gapminder",
+             "PNWColors",
+             "arsenal",
+             "knitr",
+             "rmarkdown",
+             "tinytex",
+             "kableExtra",
+             "texreg",
+             "flextable",
+             "tableone",
+             "gt",
+             "gtsummary",
+             "bookdown",
+             "rmdformats",
+             "rticles",
+             "bslib",
+             "thematic",
+             "pagedown",
+             "xaringan",
+             "xaringanExtra",
+             "xaringanthemer",
+             "blogdown",
+             "distill")
 package.check <- lapply(packages, FUN = function(x){
   if (!require(x, character.only = TRUE)){
     install.packages(x, dependencies = TRUE)
@@ -21,8 +44,73 @@ package.check <- lapply(packages, FUN = function(x){
   }
 })
 
+#install_tinytex()
+
 getwd()
 rm(list=ls())
-#file.edit("xxx")
-#source(here("cleaning","xxx.R"))
-#load(here("statistical_analysis","xxx.R"))
+
+# Data import -------------------------------------------------------------
+
+data <- read_csv("input/analysis_data.csv")
+
+data %>% glimpse()
+
+col <- data %>% colnames()
+
+# Table 1 -------------------------------------------------------------------
+
+vars <- c("phase",
+          "intervention",
+          "grants_cat",
+          "citation_number",
+          "patients",
+          "statistician",
+          "consort",
+          "interation_pair",
+          "interaction_outcome",
+          "pre_registration",
+          "non_linear_regression",
+          "method",
+          "multiplicity",
+          "sugbroup_significance",
+          "spin",
+          "interpretation",
+          "methodology")
+factorVars <- c("phase",
+                "intervention",
+                "grants_cat",
+                "statistician",
+                "consort",
+                "interaction_outcome",
+                "non_linear_regression",
+                "method",
+                "multiplicity",
+                "sugbroup_significance",
+                "spin",
+                "interpretation",
+                "methodology")
+table1 <- CreateTableOne(vars = vars,
+                         data = data,
+                         includeNA = TRUE,
+                         factorVars = factorVars)
+table1 %>% 
+  print(nonnormal = c("citation_number",
+                      "patients",
+                      "interation_pair")) %>% 
+  write.csv("table1.csv")
+
+# Table 2 -----------------------------------------------------------------
+
+data2 <- data %>% 
+  mutate(methodology = if_else(methodology == 2 | methodology == 0, 0, 1))  
+
+table2 <- CreateTableOne(vars = vars,
+                         data = data2,
+                         includeNA = FALSE,
+                         strata = "methodology",
+                         factorVars = factorVars)
+table2 %>% 
+  print(nonnormal = c("citation_number",
+                      "patients",
+                      "interation_pair"))  %>% 
+  write.csv("table2.csv")
